ad81c0280aa7d39261534b8014e790824b102b93
Michael Bisgaard Olesen <mo@concordium.com>
2022-09-20 10:37:46 +0200

    dApp: Add description component to create page

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
-import {Col, Container, Form, Nav, Navbar, Row} from "react-bootstrap";
+import {Navbar} from "react-bootstrap";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import CreateElectionPage from "./CreateElectionPage";
 import VotePage from "./VotePage";
 import Results from "./Results";
 
 function App() {
-  const [description, setDescription] = useState();
-
   return (
       <Router>
         <Navbar />
@@ -20,32 +16,6 @@ function App() {
           <Route path='/results' element={<Results/>} />
         </Routes>
       </Router>
-
-      // <>
-      //   <Nav>
-      //     <NavMenu>
-      //       <NavLink to="/create" activeStyle>Create</NavLink>
-      //       <NavLink to="/vote" activeStyle>Vote</NavLink>
-      //       <NavLink to="/results" activeStyle>Results</NavLink>
-      //     </NavMenu>
-      //   </Nav>
-      // </>
-
-      // <Container>
-      //   <Row>
-      //     <Col>
-      //         <Form>
-      //           <Form.Group className="mb-3">
-      //             <Form.Label>Description</Form.Label>
-      //             <Form.Control type="" placeholder="Enter email" />
-      //             <Form.Text className="text-muted">
-      //               We'll never share your email with anyone else.
-      //             </Form.Text>
-      //           </Form.Group>
-      //         </Form>
-      //     </Col>
-      //   </Row>
-      // </Container>
   );
 }
 
--- dapp/src/CreateElectionPage.js
+++ dapp/src/CreateElectionPage.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Col, Container, FloatingLabel, Form, Row} from "react-bootstrap";
 
 const CreateElectionPage = () => {
+    const [description, setDescription] = useState();
     return (
-        <div>
-            <h1>Create election</h1>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <Form>
+                        <FloatingLabel label="Enter description of election.">
+                            <Form.Control
+                                as="textarea"
+                                style={{height: '100px'}}
+                                value={description}
+                                onChange={e => setDescription(e.target.value)}
+                            />
+                        </FloatingLabel>
+                    </Form>
+                    <div>{description}</div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
