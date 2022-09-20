4a9e30c79eaabd1175d490d257c9791119c4bcfa
Michael Bisgaard Olesen <mo@concordium.com>
2022-09-20 10:25:08 +0200

    dApp: Add pages

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {Col, Container, Form, Nav, Navbar, Row} from "react-bootstrap";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import CreateElectionPage from "./CreateElectionPage";
+import VotePage from "./VotePage";
+import Results from "./Results";
 
 function App() {
+  const [description, setDescription] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/*<Route exact path='/' exact element={<Home />} />*/}
+          <Route path='/create' element={<CreateElectionPage/>} />
+          <Route path='/vote' element={<VotePage/>} />
+          <Route path='/results' element={<Results/>} />
+        </Routes>
+      </Router>
+
+      // <>
+      //   <Nav>
+      //     <NavMenu>
+      //       <NavLink to="/create" activeStyle>Create</NavLink>
+      //       <NavLink to="/vote" activeStyle>Vote</NavLink>
+      //       <NavLink to="/results" activeStyle>Results</NavLink>
+      //     </NavMenu>
+      //   </Nav>
+      // </>
+
+      // <Container>
+      //   <Row>
+      //     <Col>
+      //         <Form>
+      //           <Form.Group className="mb-3">
+      //             <Form.Label>Description</Form.Label>
+      //             <Form.Control type="" placeholder="Enter email" />
+      //             <Form.Text className="text-muted">
+      //               We'll never share your email with anyone else.
+      //             </Form.Text>
+      //           </Form.Group>
+      //         </Form>
+      //     </Col>
+      //   </Row>
+      // </Container>
   );
 }
 
---
+++ dapp/src/CreateElectionPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateElectionPage = () => {
+    return (
+        <div>
+            <h1>Create election</h1>
+        </div>
+    );
+};
+
+export default CreateElectionPage;
