9f6c72df835e59928161a3ddc770c28f70b950cb
Michael Bisgaard Olesen <mo@concordium.com>
2022-09-20 13:06:39 +0200

    dApp: Implement contract init

--- dapp/src/CreateElectionPage.js
+++ dapp/src/CreateElectionPage.js
@@ -1,6 +1,10 @@
+/* global BigInt */
 import React, {useEffect, useState} from 'react';
 import {Alert, Button, Col, Container, FloatingLabel, Form, InputGroup, Row} from "react-bootstrap";
 import {detectConcordiumProvider, WalletApi} from "@concordium/browser-wallet-api-helpers";
+import {AccountTransactionType, GtuAmount, ModuleReference} from "@concordium/web-sdk";
+import {CONTRACT_NAME, MODULE_REF, RAW_SCHEMA_BASE64} from "./config";
+import moment from "moment";
 
 async function addOption(options, setOptions, newOption, setOptionInput) {
     if (options.includes(newOption)) {
@@ -17,8 +21,35 @@ async function connect(client: WalletApi, setConnectedAccount) {
     return setConnectedAccount(account);
 }
 
+async function initContract(client, contractName, description, options, deadlineMinutesInput, moduleRef, senderAddress) {
+    const deadlineMinutes = Number.parseInt(deadlineMinutesInput);
+    const deadlineTimestamp = moment().add(deadlineMinutes, 'm').format();
+
+    const parameter = {
+        description: {
+            description_text: description,
+            options,
+        },
+        end_time: deadlineTimestamp,
+    };
+
+    const txHash = await client.sendTransaction(
+        senderAddress,
+        AccountTransactionType.InitializeSmartContractInstance,
+        {
+            amount: new GtuAmount(BigInt(0)),
+            moduleRef: new ModuleReference(moduleRef),
+            contractName,
+            maxContractExecutionEnergy: BigInt(30000),
+        },
+        parameter,
+        RAW_SCHEMA_BASE64,
+    );
+    console.log({txHash});
+}
+
 const CreateElectionPage = () => {
-    const [description, setDescription] = useState();
+    const [description, setDescription] = useState("");
     const [options, setOptions] = useState([]);
     const [optionInput, setOptionInput] = useState("");
     const [deadlineMinutesInput, setDeadlineMinutesInput] = useState("30");
@@ -73,6 +104,7 @@ const CreateElectionPage = () => {
             </Row>
             <Row>
                 <Col>
+                    <h2>Description</h2>
                     <FloatingLabel label="Enter description of election.">
                         <Form.Control
                             as="textarea"
@@ -87,24 +119,33 @@ const CreateElectionPage = () => {
                         e.preventDefault();
                         addOption(options, setOptions, optionInput, setOptionInput).catch(console.error);
                     }}>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                placeholder="Option"
-                                value={optionInput}
-                                onChange={e => setOptionInput(e.target.value)}
-                            />
-                            <Button type="submit" variant="outline-secondary">Add</Button>
-                        </InputGroup>
-                        <Button type="text" variant="outline-secondary" onClick={() => setOptions([])}>Clear</Button>
+                        <Row>
+                            <Col sm={10}>
+                                <InputGroup className="mb-3">
+                                    <Form.Control
+                                        placeholder="Option"
+                                        value={optionInput}
+                                        onChange={e => setOptionInput(e.target.value)}
+                                    />
+                                    <Button type="submit" variant="outline-secondary">Add</Button>
+                                </InputGroup>
+                            </Col>
+                            <Col sm={1}>
+                                <Button type="text" variant="outline-secondary"
+                                        onClick={() => setOptions([])}>Clear</Button>
+                            </Col>
+                        </Row>
                     </Form>
                     <h2>Deadline</h2>
-                    <InputGroup className="mb-3">
-                        <Form.Control
-                            placeholder="Number of minutes."
-                            value={deadlineMinutesInput}
-                            onChange={e => setDeadlineMinutesInput(e.target.value)}
-                        />
-                    </InputGroup>
+                    <Form.Control
+                        placeholder="Number of minutes."
+                        value={deadlineMinutesInput}
+                        onChange={e => setDeadlineMinutesInput(e.target.value)}
+                    />
+                    <Button
+                        className="w-100"
+                        onClick={() => initContract(client, CONTRACT_NAME, description, options, deadlineMinutesInput, MODULE_REF, connectedAccount).catch(console.error)}
+                    >Create election</Button>
                 </Col>
             </Row>
         </Container>
